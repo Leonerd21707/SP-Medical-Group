f7f13d403d67a48486e8fd6a764d45e4d2f97053
Leonardo Pereira da Rocha <51963902866@info.local>
2019-03-07 11:11:09 -0300

    inserindo alguns bugs

---
+++ Banco/MEDICALGROUP_DEV_MANHA_VIEW.sql
@@ -0,0 +1,17 @@
+USE SP_MEDIC_GROUP_DEV_MANHA
+
+CREATE VIEW sp_medgroup
+AS 
+SELECT ID, NOME
+FROM ESPECIALIZACAO
+
+SELECT * FROM ESPECIALIZACAO
+
+select * from TIPO_USUARIO
+
+select * from CONSULTA
+
+
+
+--insert into TIPO_USUARIO values ('Administrador'), ('Paciente'), ('Médico')
+
